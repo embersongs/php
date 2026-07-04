b3621a59ad1d1a5c2f358b8a66509910daa03d52
Embersong <evebay@mail.ru>
2026-07-04 15:32:59 +0700

    БД категории

--- lesson37-38/database/seeders/DatabaseSeeder.php
+++ lesson37-38/database/seeders/DatabaseSeeder.php
@@ -5,6 +5,7 @@ namespace Database\Seeders;
 use App\Models\User;
 use Illuminate\Database\Console\Seeds\WithoutModelEvents;
 use Illuminate\Database\Seeder;
+use Illuminate\Support\Facades\DB;
 
 class DatabaseSeeder extends Seeder
 {
@@ -15,11 +16,33 @@ class DatabaseSeeder extends Seeder
      */
     public function run(): void
     {
+        $categories = [
+            [
+                'name' => 'PHP',
+                'slug' => 'php',
+                'description' => 'Основы и практика PHP',
+            ],
+             [
+                'name' => 'Laravel',
+                'slug' => 'laravel',
+                'description' => 'Фреймворк Laravel',
+            ],
+            [
+                'name' => 'Оптимизация',
+                'slug' => 'optimization',
+                'description' => 'Производительность и качество кода',
+            ],
+        ];
+
+        //DB::table('categories')->truncate();
+        DB::table('categories')->insert($categories);
+
+
         // User::factory(10)->create();
 
-        User::factory()->create([
+/*        User::factory()->create([
             'name' => 'Test User',
             'email' => 'test@example.com',
-        ]);
+        ]);*/
     }
 }
