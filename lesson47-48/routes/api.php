169bc9032c3ded6c3bc1baf13c70749a2f0ef310
Embersong <evebay@mail.ru>
2026-07-26 12:56:47 +0700

    php 49-50 init

--- lesson47-48/routes/api.php
+++ lesson47-48/routes/api.php
@@ -3,6 +3,4 @@
 use Illuminate\Http\Request;
 use Illuminate\Support\Facades\Route;
 
-Route::get('/user', function (Request $request) {
-    return response()->json(\App\Models\User::all());
-});
+
